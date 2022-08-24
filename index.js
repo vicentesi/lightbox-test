aee4c29fd7448ae9b1c6e2c059d3bafd535d18bd
Vicente Silveira Inácio <silveira.vicente@gmail.com>
2022-08-24 15:34:56 -0300

    Update rotate function

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function rotateImage(idimg) {
   var img = document.getElementById(idimg);
+  var width = img.width;
+  var height = img.height;
+  var scale = height < width ? height / width : width / height;
 
-  if (img.style.transform.length) {
+  if (img.style.transform.length > 0) {
     var deg = parseInt(img.style.transform.split("(")[1]);
     deg += 90;
 
-    img.style.transform = "rotate(" + deg + "deg)";
+    if (deg % 180) {
+      img.style.transform = "rotate(" + deg + "deg) scale(" + scale + ")";
+    } else {
+      img.style.transform = "rotate(" + deg + "deg) scale(1)";
+    }
   } else {
-    img.style.transform = "rotate(90deg)";
+    img.style.transform = "rotate(90deg) scale(" + scale + ")";
   }
 }
